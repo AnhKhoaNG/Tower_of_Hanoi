f7c704532f021736d08710ae126667d0590f9e58
Khoa Nguyen <124442456+AnhKhoaNG@users.noreply.github.com>
2023-10-29 22:29:02 -0500

    Add files via upload

---
+++ hanoi.py
@@ -0,0 +1,67 @@
+moves=0
+answer = []
+import cmd
+def main():
+    
+    disks = int(input("Enter your number of disks:"))
+    tower_of_hanoi(disks,"Start","Dest","A1","A2","A3")
+    for i in range(len(answer)):
+        print(answer[i])
+    print("Total moves:", moves)
+def tower_of_hanoi(disk, start,dest,a1,a2,a3):
+    if disk ==1:
+        move(disk,start,dest)
+    elif disk >=2:
+        start_to_a3(disk-1,start,dest,a1,a2,a3)
+        move(disk,start,dest)
+        a3_to_dest(disk-1,a3,a2,a1,dest)
+        
+def start_to_a3(disk,start,dest,a1,a2,a3):
+    if disk==1:
+        move(disk,start,dest)
+        move(disk,dest,a1)
+        move(disk,a1,a2)
+        move(disk,a2,a3)
+    elif disk >=2:
+        start_to_a3(disk-1,start,dest,a1,a2,a3)
+        move(disk,start,dest)
+        move(disk,dest,a1)
+        move(disk,a1,a2)
+        a3_to_dest(disk-1,a3,a2,a1,dest)
+        move(disk,a2,a3)
+        dest_to_a3(disk-1,dest,a1,a2,a3)
+def a3_to_dest(disk,a3,a2,a1,dest):
+    if disk ==1:
+        move(disk,a3,a2)
+        move(disk,a2,a1)
+        move(disk,a1,dest)
+    elif disk >=2:
+        a3_to_dest(disk-1,a3,a2,a1,dest)
+        move(disk,a3,a2)
+        move(disk,a2,a1)
+        dest_to_a3(disk-1,dest,a1,a2,a3)
+        move(disk,a1,dest)
+        a3_to_dest(disk-1,a3,a2,a1,dest)
+        
+def dest_to_a3(disk,dest,a1,a2,a3):
+    if disk ==1:
+        move(disk,dest,a1)
+        move(disk,a1,a2)
+        move(disk,a2,a3)
+    elif disk >=2:
+        dest_to_a3(disk-1,dest,a1,a2,a3)
+        move(disk,dest,a1)
+        move(disk,a1,a2)
+        a3_to_dest(disk-1,a3,a2,a1,dest)
+        move(disk,a2,a3)
+        dest_to_a3(disk-1,dest,a1,a2,a3)
+        
+
+def move(disk,start,dest):
+    global moves
+    moves+=1
+    global answer
+    ans =("Move "+ str(moves)+ ": Moves disk "+ str(disk)+ " from "+str(start)+ " to "+str(dest))
+    answer.append(ans)
+if __name__ =="__main__":
+    main()
